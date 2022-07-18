99a3539fff7943cace304e1b05983ac2b5ae69ca
Felix Pan <tinylambda@gmail.com>
2022-07-18 13:38:29 +0800

    Test column

--- dags/copy_db.py
+++ dags/copy_db.py
@@ -4,6 +4,7 @@ from datetime import datetime, timedelta
 from airflow import DAG, macros
 from airflow.operators.python import PythonOperator
 from airflow.providers.mysql.hooks.mysql import MySqlHook
+from airflow.models import Variable
 
 
 def start(ds=None, **kwargs):
@@ -14,19 +15,32 @@ def end(ds=None, **kwargs):
     return f"End execution on {ds}"
 
 
+def get_sync_table_names():
+    sync_tables: str = Variable.get("script_tables_for_sync", None)
+    if sync_tables is not None and sync_tables.strip():
+        sync_tables = sync_tables.strip()
+        sync_table_names = [item.strip() for item in sync_tables.split()]
+    else:
+        mh_default = MySqlHook()
+        all_tables = mh_default.get_records("show tables in eano")
+        sync_table_names = [item[0] for item in all_tables]
+    return sync_table_names
+
+
 def copy_db(ds=None, **kwargs):
     mh_default = MySqlHook()
     mh_test = MySqlHook(mysql_conn_id="mysql_test")
 
-    tables = mh_default.get_records("show tables")
-    logging.info("tables: %s", tables)
+    sync_table_names = get_sync_table_names()
+    sync_table_names.insert(0, "xxxxx")
 
-    for table in tables:
-        table_name = table[0]
+    for table_name in sync_table_names:
         logging.info("processing table: %s", table_name)
-        columns = mh_default.get_records(f"show columns from {table_name}")
-        for column in columns:
-            logging.info("column: %s", column)
+        try:
+            columns = mh_default.get_records(f"show columns from {table_name}")
+        except Exception as e:
+            logging.error("catch error", exc_info=e)
+            raise e
 
 
 with DAG(
